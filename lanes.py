b7c20d58fd8fd4ee3e57f01973719dc2ee499732
Xavier Naranjo <xaviernaranjo1130@gmail.com>
2019-11-27 21:33:32 -0500

    finally combining the images to show lines and lanes
    Signed-off-by: Xavier Naranjo <xaviernaranjo1130@gmail.com>

--- lanes.py
+++ lanes.py
@@ -9,6 +9,7 @@ import cv2
 import numpy as np
 
 
+# noinspection PyUnusedLocal
 def canny(image):
     """
     :param image: regular picture
@@ -18,6 +19,19 @@ def canny(image):
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
+
+
+def display_lines(image, lines):
+    """
+    :param image: to detect lines
+    :param lines:detecting
+    """
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
 
 
 def region_of_interest(image):
@@ -39,5 +53,9 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
-cv2.imshow('result', cropped_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]),
+                        minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('result', combo_image)
 cv2.waitKey(0)
