31ac515484e3c83943ac8b9d0b620a4417951cc4
Xavier Naranjo <xaviernaranjo1130@gmail.com>
2019-11-27 19:18:13 -0500

    Smooth image with Gaussian blur

--- lanes.py
+++ lanes.py
@@ -1,6 +1,7 @@
 """
 self driving course i found online
-https://www.udemy.com/course/applied-deep-learningtm-the-complete-self-driving-car-course/learn/lecture/11241778#content
+https://www.udemy.com/course/applied-deep-learningtm-the-complete-self-
+driving-car-course/learn/lecture/11241778#content
 _author_ Xavier Naranjo 11/27/2019
 using anaconda 3.7 download
 """
@@ -10,5 +11,6 @@ import numpy as np
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-cv2.imshow('result', gray)
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imshow('result', blur)
 cv2.waitKey(0)
