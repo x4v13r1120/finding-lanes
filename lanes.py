950f8bbe337551d591b12601fbda1ba1f2275d68
Xavier Naranjo <xaviernaranjo1130@gmail.com>
2019-11-27 20:16:43 -0500

    created a mask of the lanes into a polygon or triangle to fill the mask as a region of intrest or "lane"
    Signed-off-by: Xavier Naranjo <xaviernaranjo1130@gmail.com>

--- lanes.py
+++ lanes.py
@@ -8,10 +8,34 @@ using anaconda 3.7 download
 import cv2
 import numpy as np
 
+
+def canny(image):
+    """
+    :param image: regular picture
+    :return: canny image
+    """
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+
+def region_of_interest(image):
+    """
+    :param image: creates triangle within image
+    had to change to polygon because of an exception where fill poly fills an
+    area of polygons instead of an array to one polygon
+    """
+    height = image.shape[0]
+    polygons = np.array([
+        [(200, height), (1100, height), (550, 250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
+
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-canny = cv2.Canny(blur, 50, 150)
-cv2.imshow('result', canny)
+canny = canny(lane_image)
+cv2.imshow('result', region_of_interest(canny))
 cv2.waitKey(0)
